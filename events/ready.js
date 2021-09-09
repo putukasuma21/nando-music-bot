e797aee1d0879246144d54b75e7b2c3a434267b8
Putukasuma21 <kasuma.ananda1998@hotmail.com>
2021-09-09 16:32:44 +0800

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -3,7 +3,7 @@ module.exports = async (client) => {
   client.user.setPresence({
     status: "online",  // You can show online, idle, and dnd
     activity: {
-        name: "Naandooh 🎉",  // The message shown
+        name: "-help, Naandooh 🎉",  // The message shown
         type: "LISTENING", // PLAYING, WATCHING, LISTENING, STREAMING,
     }
 });
